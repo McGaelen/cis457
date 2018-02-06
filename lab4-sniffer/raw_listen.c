8e1fc97a67d55fd82f245090e715b1b207264bd8
Gaelen McIntee <mcinteeg@mail.gvsu.edu>
2018-02-06 13:10:02 -0800

    finished lab 4

--- lab4-sniffer/raw_listen.c
+++ lab4-sniffer/raw_listen.c
@@ -5,6 +5,8 @@
 #include <netpacket/packet.h>
 #include <net/ethernet.h>
 #include <netinet/ether.h>
+#include <netinet/ip.h>
+#include <arpa/inet.h>
 // net/ has lower lever stuff
 // netinet/ has higher level stuff
 
@@ -23,10 +25,23 @@ int main(int argc, char **argv) {
 		int n = recvfrom(sockfd, buf, 5000, 0, (struct sockaddr *) &addr, &len);
 
 		if (addr.sll_pkttype != PACKET_OUTGOING) {
-			printf("Got a packet\n");
+			printf("\nGot a packet\n");
 			struct ether_header eh;
 			memcpy(&eh, &buf[0], 14);
 			printf("Destination: %s\n", ether_ntoa((struct ether_addr *) &eh.ether_dhost) );
+			printf("Source: %s\n", ether_ntoa((struct ether_addr *) &eh.ether_shost) );
+			printf("Type: %d\n", eh.ether_type);
+
+			if (ntohs(eh.ether_type) == ETHERTYPE_IP) {
+				struct iphdr ip;
+				memcpy(&ip, &buf[14], 60);
+				struct in_addr dest;
+				dest.s_addr = ip.daddr;
+				struct in_addr src;
+				src.s_addr = ip.saddr;
+				printf("IP Dest: %s\n", inet_ntoa(dest));
+				printf("IP Source: %s\n", inet_ntoa(src));
+			}
 		}
 	}
 }
